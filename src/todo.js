56e269b5c5a3d0d4348bc7659adaf4058bacba89
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-09-27 23:43:41 -0500

    Update todo.js

--- src/todo.js
+++ src/todo.js
@@ -21,7 +21,7 @@ export default function Todo({user, title, create_date, description}) {
             <p>Create by: {user}</p>
             <p>Description: {description}</p>
             <p> Date: {create_date} </p>
-            <p>  <input type='checkbox' onChange={checkboxStatus} /> Complete</p>
+            <p>  <input type='checkbox' value = {status} onChange={checkboxStatus} /> Complete</p>
             <p> Date Completed: {completeDate()} </p>
         </div>
     )
