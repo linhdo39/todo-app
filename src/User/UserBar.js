f41758c2b271c1324c3dbc84ea3825df9d777ea6
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-10-02 01:58:05 -0500

    Update UserBar.js

--- src/User/UserBar.js
+++ src/User/UserBar.js
@@ -2,7 +2,7 @@ import Logout from './logout'
 import Registration from './registration'
 import Login from './login'
 
-export default function Todolist() {
+export default function User() {
      const user='Linh'
      if (user) {
           return <Logout user={user} />
