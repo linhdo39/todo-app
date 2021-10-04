70eb6e65eca329bdbe3d7ad6b8a0ad154b39068c
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-10-04 01:29:50 -0500

    Update registration.js

--- src/User/registration.js
+++ src/User/registration.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function Registration() {
-	return (
-		<form onSubmit={e => e.preventDefault()}>
-		    <div> <h3> Registration </h3> </div>
-		    <div>
-			    <label htmlFor="sign-up-username">Username:</label>
-			    <input type="text" name="sign-up-username" id="sign-up-username" />
-			</div>
-			    <label htmlFor="sign-up-password">Password:</label>
-			    <input type="password" name="sign-up-password" id="sign-up-password" />
-			<div>
-			    <label htmlFor="sign-up-password-repeat">Repeat password:</label>
-			    <input type="password" name="sign-up-password-repeat" id="sign-up-password-repeat" />
-			</div>
-			<input type="submit" value="Register" />
-		</form>
-	)
+export default function Registration({dispatchUser}) {
+	const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+        passwordRepeat: ""
+    })
+     return (
+          <form onSubmit={e => {e.preventDefault(); dispatchUser({type:"REGISTRATION", username:formData.username}); }}>
+              <label htmlFor="sign-up-username">Username:</label>
+              <input type="text" value={formData.username} onChange={e => setFormData({...formData, username: e.target.value})}  name="sign-up-username" id="sign-up-username" />
+
+              <label htmlFor="sign-up-password">Password:</label>
+              <input type="password" value={formData.password} onChange={e => setFormData({...formData, password: e.target.value})} name="sign-up-password" id="sign-up-password" />
+
+              <label htmlFor="password-repeat">Repeat password:</label>
+              <input type="password" value={formData.passwordRepeat} onChange={e => setFormData({...formData, passwordRepeat: e.target.value})}  name="password-repeat" id="password-repeat" />
+
+              <input type="submit" value="Register" disabled={formData.username.length === 0 || formData.password.length === 0 || formData.password !== formData.passwordRepeat}  />
+          </form>
+      )
 }
