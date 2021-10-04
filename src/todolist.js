023c7ad4cdc8c8a2ab389da139130d2ce5a1fbf7
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-10-04 01:29:44 -0500

    Update todolist.js

--- src/todolist.js
+++ src/todolist.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Todo from './todo'
 
-export default function Todolist({Todos = []}) {
+export default function Todolist({Todos = [],dispatch}) {
     return (
         <div>
-            {Todos.map((p, i) => <Todo {...p} author={p.author} title={p.title} description ={p.description}  key={'todo-' + i} />)}
+            {Todos.map((p, i) => <Todo {...p} key = {p.id}
+                                              id= {p.id}
+                                              user= {p.user}
+                                              title={p.title}
+                                              create_date ={p.create_date}
+                                              description ={p.description}
+                                              completed= {p.completed}
+                                              completed_date ={p.completed_date}
+                                              dispatch={dispatch}
+                                              />)}
         </div>
     )
 }
