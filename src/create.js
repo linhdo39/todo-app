ae3a57e49c9dd9c7ec71d866dcfeb1bea0909065
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-10-04 01:29:36 -0500

    Update create.js

--- src/create.js
+++ src/create.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function Create ({user}) {
+export default function Create ({user, todos, dispatch}) {
+     const [ title, setTitle ] = useState('')
+     const [ description, setDescription ] = useState('')
+
+     function titleHandler (event) {
+        setTitle(event.target.value)
+     }
+
+     function descriptionHandler (event) {
+        setDescription(event.target.value)
+     }
+
+     function createHandler() {
+        dispatch({type: 'CREATE_TODO', id:new Date().getTime() +Math.random(), user, title, create_date: Date(Date.now()).toString(), description, completed:false, completed_date: ''})
+     }
+
      return (
-           <form onSubmit={e => e.preventDefault()}>
+           <form onSubmit={event => {event.preventDefault(); createHandler();} }>
               <br/>
               <div>User: <b>{user}</b></div>
                    <div>
                        <label htmlFor="create-title">Title:</label>
-                       <input type="text" name="create-title" id="create-title" />
+                       <input type="text" value={title} onChange={titleHandler} name="create-title" id="create-title" />
                    </div>
                    <div>
                        <label htmlFor="create-description">Description: </label>
-                       <textarea />
+                       <textarea value={description} onChange={descriptionHandler}/>
                    </div>
-                   {/*(}<div>
-                      <label htmlFor="create-date"> Create Date: {Date(Date.now()).toString()}</label>
-                   </div>)*/}
+
               <input type="submit" value="Create" />
            </form>
           )
