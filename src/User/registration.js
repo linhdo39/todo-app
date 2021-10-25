3d6033a538bf6facc9e5bda69edae54a1b881c60
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-10-24 23:08:29 -0500

    Update registration.js

--- src/User/registration.js
+++ src/User/registration.js
@@ -1,13 +1,32 @@
-import React, {useState} from 'react'
+import React, {useContext, useState} from 'react'
+import { StateContext } from '../contexts';
+import { useEffect } from 'react';
+import { useResource } from 'react-request-hook';
 
-export default function Registration({dispatchUser}) {
+export default function Registration() {
+
+    const {dispatch} = useContext(StateContext)
+
 	const [formData, setFormData] = useState({
         username: "",
         password: "",
         passwordRepeat: ""
     })
-     return (
-          <form onSubmit={e => {e.preventDefault(); dispatchUser({type:"REGISTRATION", username:formData.username}); }}>
+    
+    const [ user, register ] = useResource((username, password) => ({
+        url: '/users',
+        method: 'post',
+        data: { username, password }
+    }))
+
+    useEffect(() => {
+        if (user && user.data) {
+            dispatch({ type: 'REGISTRATION', username: user.data.username })
+        }
+      }, [user])
+
+    return (
+          <form onSubmit={e => {e.preventDefault(); register(formData.username, formData.password); }}>
               <label htmlFor="sign-up-username">Username:</label>
               <input type="text" value={formData.username} onChange={e => setFormData({...formData, username: e.target.value})}  name="sign-up-username" id="sign-up-username" />
 
@@ -19,5 +38,5 @@ export default function Registration({dispatchUser}) {
 
               <input type="submit" value="Register" disabled={formData.username.length === 0 || formData.password.length === 0 || formData.password !== formData.passwordRepeat}  />
           </form>
-      )
+    )
 }
