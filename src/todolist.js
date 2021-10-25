022e73327077c654309de223312e88194c012e05
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-10-24 23:08:21 -0500

    Update todolist.js

--- src/todolist.js
+++ src/todolist.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Todo from './todo'
 
-export default function Todolist({Todos = [],dispatch}) {
+import { StateContext } from './contexts'
+
+
+export default function Todolist() {
+    const {state} = useContext(StateContext)
+    const {todos} = state;
     return (
-        <div>
-            {Todos.map((p, i) => <Todo {...p} key = {p.id}
-                                              id= {p.id}
+       <div>
+            {todos.map((p, i) => <Todo {...p} id={p.id}
                                               user= {p.user}
                                               title={p.title}
                                               create_date ={p.create_date}
                                               description ={p.description}
                                               completed= {p.completed}
                                               completed_date ={p.completed_date}
-                                              dispatch={dispatch}
+                                              key={'Todo-' + i} 
                                               />)}
         </div>
     )
