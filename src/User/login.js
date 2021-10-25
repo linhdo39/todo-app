77e262bbc8593041657e8f3b009ebae97960a8ad
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-10-24 23:08:24 -0500

    Update login.js

--- src/User/login.js
+++ src/User/login.js
@@ -1,14 +1,43 @@
-import React, {useState} from 'react'
+import React, {useContext, useState} from 'react'
+import { useResource } from 'react-request-hook';
+import { StateContext } from '../contexts';
+import { useEffect} from 'react';
 
-export default function Login({dispatchUser}){
-	const[username, setUsername] = useState('');
 
-	function userNameHandler (event) {
-    	    setUsername(event.target.value)
-   	}
+export default function Login(){
+	const {dispatch} = useContext(StateContext)
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('')
+    const [failed, setFailed] = useState(false)
+
+    function userNameHandler (evt) { 
+		setUsername(evt.target.value) 
+	} 
+    
+	function passwordHandler (evt) { 
+		setPassword(evt.target.value) 
+	}
+
+    const [ user, login ] = useResource((username, password) => ({
+        url: `/login/${encodeURI(username)}/${encodeURI(password)}`,
+        method: 'get'
+    }))
+
+    useEffect(() => {
+        if (user && user.data) {
+        console.log(user)
+            if (user.data.length > 0) {
+                            setFailed(false)
+                            dispatch({type: 'LOGIN', username: user.data[0].username })
+            } else {
+                            setFailed(true)
+            }
+        } 
+    }, [user])
 
 	return (
-		<form onSubmit ={event => {event.preventDefault(); dispatchUser({type:"LOGIN", username})} }>
+		<form onSubmit ={event => {event.preventDefault(); login(username, password);} }>
 		    <div> <h3> Login </h3> </div>
 		    <div>
 			<label htmlFor="login-username">Username:</label>
@@ -17,7 +46,8 @@ export default function Login({dispatchUser}){
 			<div>
 			<label htmlFor="login-password">Password:</label>
 			</div>
-			<input type="password" name="login-password" id="login-password" />
+			<input type="password" value={password} onChange={passwordHandler} name="login-password" id="login-password" />
+			{failed && <p style={{ color: 'red' }}>Invalid username or password</p>}
 			<div>
 			<input type="submit" value="Login" />
 			</div>
