f4c16f9ac4c10009a64228f5635fed3604f7f1e2
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-11-18 00:10:33 -0600

    Update UsersMap.js

--- src/User/UsersMap.js
+++ src/User/UsersMap.js
@@ -1,13 +1,18 @@
 import React, { useContext } from 'react'
 import { IndividualUser } from './IndividualUser';
-
+import Todo from '../Todo';
 import { StateContext } from '../Contexts'
 
 export default function UsersMap () {   
     const {state} = useContext(StateContext)
-    const {users} = state;  
-    return users.map((p,i) => <IndividualUser {...p} id={p.id}
-                                            username= {p.username}
-                                            key ={i}/>)
+    const {users} = state;
+    return (
+       <div>
+            {users.map((p, i) => <IndividualUser {...p} id={p.id}
+                                              username= {p.username}
+                                              key={'user' + i} 
+                                              />)}
+        </div>
+    )
 
 }
