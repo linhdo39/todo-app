a94882d81280b038dd1afae4230732b3a023aa42
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-11-18 00:10:30 -0600

    Update Users.js

--- src/User/Users.js
+++ src/User/Users.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { useContext, useEffect } from 'react';
-import { StateContext} from '../Contexts'
-import { useResource } from 'react-request-hook';
-import UsersMap from './UsersMap';
-import { Link } from 'react-navi';
 
-export default function Users () {
-  const { state, dispatch } = useContext(StateContext)
+import React, {useEffect, useContext} from 'react'
+import { StateContext } from '../Contexts'
+import { useResource } from 'react-request-hook'
+import Todolist from '../Todolist'
+import UsersMap from './UsersMap'
+
+export default function HomePage () {
+    const { state, dispatch } = useContext(StateContext)
     
-  const [ users, getUsers ] = useResource(() => ({
-      url: '/users',
-      method: 'get'
-    }))
+    const [ users, getUsers ] = useResource(() => ({
+        url: '/users',
+        method: 'get'
+      }))
+      
+    useEffect(getUsers, [])
+    useEffect(() => {  
+        if (users && users.data) 
+        dispatch({ type: 'USERS', users: users.data})
+    }, [users])
 
-  useEffect(getUsers, [])
-  useEffect(() => {  
-      if (users && users.data) 
-      dispatch({ type: 'GET_USERS', users: users.data.reverse()})
-  }, [users])
-
-  const { data, isLoading } = users;
-  return (
-      <>
-        {isLoading && 'Users loading...'} <UsersMap/>
-      </>
-  )
+    const { data, isLoading } = users;
+    return (
+        <>
+          {isLoading && 'Todos loading...'} <UsersMap/>
+        </>
+    )
 }
