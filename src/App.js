9c0a938e80b24281caec0ccc7b3305b13564661c
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-11-17 21:58:39 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,60 +1,51 @@
 import React, {useState, useReducer, useEffect} from 'react';
-import {useResource} from 'react-request-hook';
-import User from './User/UserBar'
-import Create from './create'
-import Todolist from './todolist'
+import HeaderBar from './Page/HeaderBar';
+import HomePage from './Page/HomePage';
+import Create from './Create';
+import TodoPage from './Page/TodoPage';
+import Profile from './User/Profile';
+import Users from "./User/Users"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row, Col} from 'react-bootstrap';
+import { mount, route } from 'navi';
+import { Router, View } from 'react-navi';
+import { StateContext, ThemeContext } from './Contexts';
 import reducer from './reducer';
-import Header from './header';
-import ChangeTheme from './changeTheme';
-import { ThemeContext, StateContext } from './contexts';
+
 
 function App() {
-    const [ todos, getTodos ] = useResource(() => ({
-        url: '/todos',
-        method: 'get'
-      }))
-    
-    const [ state, dispatch ] = useReducer(reducer, { user: '', todos: [] })
 
-    useEffect(getTodos, [])
-    
-    useEffect(() => {  
-        if (todos && todos.data) 
-        dispatch({ type: 'GET_TODOS', todos: todos.data.reverse()})
-    }, [todos])
-    
-    
-    const {user} = state;
-    useEffect(() => {
-        if (user) {
-            document.title = `${user}’s Blog` 
-        } else {
-            document.title = 'Blog'
-        }
-    }, [user])
-    
+    const [state, dispatch] = useReducer(reducer, { user: '', users: [],todos: [] })
+    const {user} = state
+
     const [ theme, setTheme ] = useState({
         primaryColor: '#CC5500',
         secondaryColor: '#E97451'
     })
-
-    const { data, isLoading } = todos;
+    
+    const routes = mount ({
+        '/': route({view:<HomePage/>}),
+        '/todo/create': route ({view:<Create/>}),
+        '/todo/:id': route (req => 
+            {return {view: <TodoPage id = {req.params.id}/>}}),
+        '/users' :route({view:<Users/>}),
+        '/users/:userId' : route (req => 
+            {return {view: <Profile username = {req.params.username}/>}}),
+    })
 
     return (
-        <div>
-            <ThemeContext.Provider value={theme}>
-                <StateContext.Provider value={{state: state, dispatch: dispatch}}>
-                    <Header text="To Do List" />
-                    <ChangeTheme theme={theme} setTheme={setTheme} />
-                    <User/>
-                    <br/><br/><hr/><br/> 
-                    {user && <Create /> }
-                    <>
-                        {isLoading && 'To do list loading...'} <Todolist />
-                    </>
-                </StateContext.Provider>
-            </ThemeContext.Provider>
-        </div>
+        <>
+        <ThemeContext.Provider value = {theme}>
+            <StateContext.Provider value={{state: state, dispatch: dispatch}}>
+            <Router routes = {routes}>
+                <Container>
+                    <HeaderBar setTheme = {setTheme}></HeaderBar>
+                    <View/>
+                </Container>
+            </Router>
+            </StateContext.Provider>
+        </ThemeContext.Provider>
+        </>
       )
 }
 
