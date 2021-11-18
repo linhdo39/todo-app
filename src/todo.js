455a226ec3a19ea5de114232ba48a76492fd0a45
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-11-17 21:59:10 -0600

    Update todo.js

--- src/todo.js
+++ src/todo.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useContext } from 'react';
-import { ThemeContext, StateContext } from './contexts'
+import { ThemeContext, StateContext } from './Contexts'
 import { useResource } from 'react-request-hook';
-export default function Todo({id,user, title, create_date, description,completed,completed_date}) {
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-navi';
+import {Button} from 'react-bootstrap'
+
+export default function Todo({id,user, title, create_date, description,completed,completed_date, short = false}) {
     
     const { secondaryColor } = useContext(ThemeContext)
     const {dispatch} = useContext(StateContext)
@@ -25,15 +29,35 @@ export default function Todo({id,user, title, create_date, description,completed
         updateTodo({completed:completed, completed_date:completed_date})
     }
 
+    let processedDescription = description
+
+     if (short) {
+          if (description.length > 30) {
+               processedDescription = description.substring(0, 30) + '...'
+          }
+     }
+
     return (
-        <div>
-            <h3 style ={{color: secondaryColor}}>{title}</h3>
-            <p><b>Create by:</b> {user}</p>
-            <p><b>Description:</b> {description}</p>
-            <p><b>Date Created:</b> {create_date} </p>
-            <p><input type='checkbox' checked={completed} onClick={e => {dispatch({type: 'TOGGLE_TODO',completed:completed, id:id, completed_date:new Date(Date.now()).toLocaleDateString('en-us') });completeHandler()}} /> Complete</p>
-            {completed && <><b>Date Completed:</b> <i>{completed_date} </i></>}
-            <p><button onClick={e =>  {dispatch({type:'DELETE_TODO', id:id});deleteTodo()}}>Delete</button> </p>
-        </div>
+        <Card>
+          <Card.Body>
+              <Card.Title><Link style={{ color: secondaryColor }} href={`/todo/${id}`}>{title}</Link>
+              </Card.Title>
+              <Card.Subtitle>
+              <p> <b>Create by:</b> {user} on {create_date} </p> 
+              </Card.Subtitle>
+              <Card.Text>
+                  {processedDescription}                  
+              </Card.Text>
+              <div><input type='checkbox' checked={completed} onClick={e => 
+                    {dispatch({type: 'TOGGLE_TODO',completed:completed, id:id, completed_date:new Date(Date.now()).toLocaleDateString('en-us') });completeHandler()}} /> 
+                 <b> Complete? </b><br/>
+                {completed && <><b>Date Completed:</b> <i>{completed_date} </i></>}
+                </div>
+                <br/>
+              <Button  style = {{background:secondaryColor}} onClick = {e =>  {dispatch({type:'DELETE_TODO', id:id});deleteTodo()}}> Delete</Button>
+              {short && <Link href={`/todo/${id}`}>View full post</Link>}
+            
+          </Card.Body>
+          </Card>
     )
 }
