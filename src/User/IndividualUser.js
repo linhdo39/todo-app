bab99c4f3551e53df0b14330aecad3e4d61a0d91
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-11-17 22:00:07 -0600

    Update IndividualUser.js

--- src/User/IndividualUser.js
+++ src/User/IndividualUser.js
@@ -7,6 +7,11 @@ import { Link } from 'react-navi';
 export function IndividualUser({id, username}){
     const { secondaryColor } = useContext(ThemeContext)
         return (
-            <div>console.log({username})</div>
+            <Card>
+              <Card.Body>
+                  <Card.Title><Link style={{ color: secondaryColor }} href={`/users/${id}`}>{username}</Link>
+                  </Card.Title>
+              </Card.Body>
+              </Card>
         )
     }
