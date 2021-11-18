353c8900ea33cbb39685daa0bb4997b4826b6568
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-11-17 21:58:51 -0600

    Update create.js

--- src/create.js
+++ src/create.js
@@ -1,11 +1,13 @@
 import React, {useContext, useState} from 'react'
-import { StateContext } from './contexts'
+import { StateContext } from './Contexts'
 import { useResource } from 'react-request-hook'
 import { useEffect } from 'react'
+import { useNavigation } from 'react-navi'
 
 export default function Create () {
    const [ title, setTitle ] = useState('')
    const [ description, setDescription ] = useState('')
+   const navigation = useNavigation()
    const {state, dispatch} = useContext(StateContext)
    const {id} = state;
    const {user} = state;
@@ -15,11 +17,11 @@ export default function Create () {
       method: 'post',
       data: {id, user, title, create_date, description, completed,completed_date }
   }))
-   
+
    function createHandler() {
       createTodo({user:user, title, create_date: new Date(Date.now()).toLocaleDateString('en-us'), description, completed:false, completed_date:undefined})
    }
-
+   
    function titleHandler (evt) { setTitle(evt.target.value) }
 
    function descriptionHandler (evt) { setDescription(evt.target.value) }
@@ -27,6 +29,8 @@ export default function Create () {
    useEffect(() => {
        if (Todo && Todo.isLoading === false && Todo.data) {
            dispatch({ type: 'CREATE_TODO', id, user: user, create_date:new Date(Date.now()).toLocaleDateString('en-us'),title: Todo.data.title, description: Todo.data.description})
+           console.log(Todo.data.id)
+           navigation.navigate('/todo/' + parseInt(Todo.data.id))
        }
    }, [Todo])
 
