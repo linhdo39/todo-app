35655c6461694af8f6af09e3e36617c0be1a4070
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-11-17 21:59:28 -0600

    Update registration.js

--- src/User/registration.js
+++ src/User/registration.js
@@ -1,12 +1,15 @@
 import React, {useContext, useState} from 'react'
-import { StateContext } from '../contexts';
+import { StateContext } from '../Contexts';
 import { useEffect } from 'react';
 import { useResource } from 'react-request-hook';
+import { Modal } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 
-export default function Registration() {
+export default function Registration({show, handleClose}) {
 
     const {dispatch} = useContext(StateContext)
-
+    
 	const [formData, setFormData] = useState({
         username: "",
         password: "",
@@ -26,17 +29,25 @@ export default function Registration() {
       }, [user])
 
     return (
-          <form onSubmit={e => {e.preventDefault(); register(formData.username, formData.password); }}>
-              <label htmlFor="sign-up-username">Username:</label>
-              <input type="text" value={formData.username} onChange={e => setFormData({...formData, username: e.target.value})}  name="sign-up-username" id="sign-up-username" />
-
-              <label htmlFor="sign-up-password">Password:</label>
-              <input type="password" value={formData.password} onChange={e => setFormData({...formData, password: e.target.value})} name="sign-up-password" id="sign-up-password" />
-
-              <label htmlFor="password-repeat">Repeat password:</label>
-              <input type="password" value={formData.passwordRepeat} onChange={e => setFormData({...formData, passwordRepeat: e.target.value})}  name="password-repeat" id="password-repeat" />
-
-              <input type="submit" value="Register" disabled={formData.username.length === 0 || formData.password.length === 0 || formData.password !== formData.passwordRepeat}  />
-          </form>
+           <Modal show={show} onHide={handleClose}>
+           <Form onSubmit={e => { e.preventDefault(); register(formData.username, formData.password); handleClose(); }}>
+             <Modal.Header closeButton>
+               <Modal.Title>Register</Modal.Title>
+             </Modal.Header>
+             <Modal.Body>
+               <Form.Label htmlFor="register-username">Username:</Form.Label>
+               <Form.Control type="text" value={formData.username} onChange={e => setFormData({ ...formData, username: e.target.value })} name="register-username" id="register-username" />
+               <Form.Label htmlFor="register-password">Password:</Form.Label>
+               <Form.Control type="password" name="register-password" id="register-password" value={formData.password} onChange={e => setFormData({ ...formData, password: e.target.value })} />
+               <Form.Label htmlFor="register-password-repeat">Repeat password:</Form.Label>
+               <Form.Control type="password" name="register-password-repeat" id="register-password-repeat" value={formData.passwordRepeat} onChange={e => setFormData({ ...formData, passwordRepeat: e.target.value })} />
+             </Modal.Body>
+             <Modal.Footer>
+               <Button variant="secondary" onClick={handleClose}>Cancel</Button>
+               <Button variant="primary" type="submit" disabled={formData.username.length === 0 || formData.password.length === 0 || formData.password !== formData.passwordRepeat}>Register</Button>
+             </Modal.Footer>
+           </Form>
+         </Modal>
+     
     )
 }
