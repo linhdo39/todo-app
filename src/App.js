8f67ed60f44c5af18e766f2cd29682db1e55f925
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-11-22 01:22:30 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import React, {useState, useReducer, useEffect} from 'react';
+import React, {useState, useReducer} from 'react';
 import HeaderBar from './Page/HeaderBar';
 import HomePage from './Page/HomePage';
-import Create from './Create';
+import Create from './Todos/Create';
 import TodoPage from './Page/TodoPage';
 import Profile from './Page/ProfilePage';
 import UsersPage from "./Page/UsersPage"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 import { mount, route } from 'navi';
 import { Router, View } from 'react-navi';
 import { StateContext, ThemeContext } from './Contexts';
 import reducer from './reducer';
-
+import DeleteTodo from './Todos/DeleteTodo';
 
 function App() {
 
-    const [state, dispatch] = useReducer(reducer, { user: '', users: [],todos: [] })
-    const {user} = state
+    const [state, dispatch] = useReducer(reducer, { user: {}, users: [], todos: [] })
 
     const [ theme, setTheme ] = useState({
+        name:'Default',
         primaryColor: '#CC5500',
-        secondaryColor: '#E97451'
+        secondaryColor: '#E97451',
     })
     
     const routes = mount ({
         '/': route({view:<HomePage/>}),
-        '/todo/create': route ({view:<Create/>}),
-        '/todo/:id': route (req => 
-            {return {view: <TodoPage id = {req.params.id}/>}}),
+        '/todos/create': route ({view:<Create/>}),
+        '/todos/:id': route (req => {
+            return { view: <TodoPage id={req.params.id} /> }}),
         '/users' :route({view:<UsersPage/>}),
         '/users/:id' : route (req => 
             {return {view:<Profile id = {req.params.id}/>}}),
+        '/todos/delete/:id': route (req => {
+            return { view: <DeleteTodo id={req.params.id} /> }}),  
     })
 
     return (
@@ -40,6 +42,7 @@ function App() {
             <Router routes = {routes}>
                 <Container>
                     <HeaderBar setTheme = {setTheme}></HeaderBar>
+                    <hr />
                     <View/>
                 </Container>
             </Router>
