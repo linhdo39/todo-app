b6da249625abc04522309d96930242aefd586037
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-11-22 01:21:32 -0600

    Update UsersPage.js

--- src/Page/UsersPage.js
+++ src/Page/UsersPage.js
@@ -2,26 +2,29 @@
 import React, {useEffect, useContext} from 'react'
 import { StateContext } from '../Contexts'
 import { useResource } from 'react-request-hook'
-import UsersMap from '../User/UsersMap'
+import UsersList from '../Users/UsersList'
 
 export default function UsersPage () {
     const { state, dispatch } = useContext(StateContext)
     
     const [ users, getUsers ] = useResource(() => ({
         url: '/users',
+        headers: {"Authorization": `${state.user.access_token}`},
         method: 'get'
       }))
       
-    useEffect(getUsers, [])
+    useEffect(getUsers, [state.user.access_token])
+
+    
     useEffect(() => {  
-        if (users && users.data) 
+        if (users.data) 
         dispatch({ type: 'USERS', users: users.data})
     }, [users])
 
     const { data, isLoading } = users;
     return (
         <>
-          {isLoading && 'Todos loading...'} <UsersMap/>
+          {isLoading && 'Users loading...'} <UsersList/>
         </>
     )
 }
