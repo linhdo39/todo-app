153f65169e72b2999ba2ecd63fc486b16a266dbe
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-11-22 01:21:38 -0600

    Update HomePage.js

--- src/Page/HomePage.js
+++ src/Page/HomePage.js
@@ -2,26 +2,31 @@
 import React, {useEffect, useContext} from 'react'
 import { StateContext } from '../Contexts'
 import { useResource } from 'react-request-hook'
-import Todolist from '../Todolist'
-
+import HomeTodoList from '../Todos/HomeTodoList'
 export default function HomePage () {
     const { state, dispatch } = useContext(StateContext)
     
     const [ todos, getTodos ] = useResource(() => ({
         url: '/todos',
-        method: 'get'
+        method: 'get',
+        headers: {"Authorization":`${state.user.access_token}`}
       }))
       
-    useEffect(getTodos, [])
+    useEffect(() => {
+        getTodos()
+    }, [state.user.access_token])
+    
+
     useEffect(() => {  
-        if (todos && todos.data) 
-        dispatch({ type: 'GET_TODOS', todos: todos.data.reverse()})
+        if (todos && todos.isLoading === false && todos.data) 
+        dispatch({ type: 'GET_TODOS', todos: todos.data.todos})
     }, [todos])
 
     const { data, isLoading } = todos;
+
     return (
-        <>
-          {isLoading && 'Todos loading...'} <Todolist/>
-        </>
-    )
+          <>
+            {isLoading && 'Loading Todos...'} <HomeTodoList/>
+          </>
+      )
 }
