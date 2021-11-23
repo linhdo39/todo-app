1d025d82bb5696b450c8008a975590a15caeb41f
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-11-23 01:10:51 -0600

    Update HomeTodoList.js

--- src/Todos/HomeTodoList.js
+++ src/Todos/HomeTodoList.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import HomeTodo from './HomeTodo';
 import { StateContext } from '../Contexts'
-
+import { Card } from 'react-bootstrap';
 
 export default function HomeTodoList() {
     const {state} = useContext(StateContext)
@@ -9,14 +9,21 @@ export default function HomeTodoList() {
     const items = todos
     return (
        <div>
-            {items.map((p, i) => <HomeTodo {...p} id={p.id}
+            {items? items.map((p, i) => <HomeTodo {...p} id={p.id}
                                               user= {p.user}
                                               short ={true}
                                               title={p.title}
                                               create_date ={p.create_date}
                                               description ={p.description}
                                               key={'Todo-' + i} 
-                                              />)}
+                                              />): <Card>
+                                              <Card.Body>
+                                                  <Card.Text>
+                                                      No Todo Items Found
+                                                  </Card.Text>
+                                
+                                              </Card.Body>
+                                              </Card>}
         </div>
     )
 }
