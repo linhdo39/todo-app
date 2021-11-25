64979b68318bcbbcab3fc2405b484ce38e5bfdb4
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-11-25 02:29:14 -0600

    Update DeleteTodo.js

--- src/Todos/DeleteTodo.js
+++ src/Todos/DeleteTodo.js
@@ -7,13 +7,15 @@ import { Link } from 'react-navi';
 export default function DeleteTodo({id}){
     console.log(id)
     const { secondaryColor } = useContext(ThemeContext)
+    const { primaryColor } = useContext(ThemeContext)
+
         return (
-            <Card>
+            <Card style = {{background:secondaryColor}}>
               <Card.Body>
                   <Card.Text>
                       This Todo item has been deleted
                   </Card.Text>
-                <Link style={{ color: secondaryColor }} href={`/users/${id}`}>Go back to Profile</Link>
+                <Link style={{ color: primaryColor }} href={`/users/${id}`}>Go back to Profile</Link>
 
               </Card.Body>
               </Card>
