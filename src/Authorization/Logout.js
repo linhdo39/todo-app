cb0441afb6f01ab216b5424433d42b569ddaeb98
My Linh Do <72121311+linhdo39@users.noreply.github.com>
2021-11-25 01:26:45 -0600

    Update Logout.js

--- src/Authorization/Logout.js
+++ src/Authorization/Logout.js
@@ -4,8 +4,9 @@ import { StateContext } from '../Contexts';
 export default function Logout() {
 	const {state,dispatch} = useContext(StateContext)
 	const {user} = state
+
 	return (
-	    <form onSubmit={e => {e.preventDefault(); dispatch({type:"LOGOUT"}) }}>
+	    <form onSubmit={e => {e.preventDefault(); dispatch({type:"LOGOUT"})}}>
             Logged in as: <b>{user.username} </b>
 	        <input type="submit" value="Logout" />
 	    </form>)
